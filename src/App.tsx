9eca025355dc06712f03a93c21ad039f62612d14
eren-the-coder <jeanpc568@gmail.com>
2025-10-18 10:14:13 +0100

    feat: implement router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,35 @@
-import Header from './components/Header/Header';
-import Main from './components/Main/Main';
-import Footer from './components/Footer/Footer';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Feed from "./pages/Feed/Feed";
+import AddPostForm from "./pages/AddPostForm/AddPostForm";
+import "./styles/global.css";
+import RootLayout from "./pages/RootLayout/RootLayout";
+import NotFound from "./pages/NotFound/NotFound";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
-import './styles/global.css';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <Feed /> },
+      { path: "contribute", element: <AddPostForm /> },
+    ],
+  },
+  {
+    path: "*",
+    element: <>
+      <Header />
+      <div className="page">
+        <NotFound />
+      </div>
+      <Footer />
+    </>,
+  },
+]);
 
 function App() {
-  return (
-    <div className="page">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
